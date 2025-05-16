b4b83fd2c4e1e547090f14a0b0616dba0f79d9c7
Nguyễn Quang Minh (NQM) <nguyenquangminh391@gmail.com>
2025-05-16 08:42:17 +0700

    feat: add info button and bottom sheet for skin details in SkinDetailScreen

--- app/src/main/java/org/nqmgaming/aneko/presentation/skin/detail/SkinDetailSreen.kt
+++ app/src/main/java/org/nqmgaming/aneko/presentation/skin/detail/SkinDetailSreen.kt
@@ -1,8 +1,10 @@
 package org.nqmgaming.aneko.presentation.skin.detail
 
 import android.net.Uri
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -10,20 +12,25 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -103,6 +110,9 @@ fun SkinDetail(
     skinConfig: SkinConfig? = null,
     skinPath: Uri? = null
 ) {
+    var isShowSkinInfoBottomSheet by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    val showSkinInfoBottomSheet = rememberModalBottomSheetState()
     Scaffold(
         modifier = modifier,
         topBar = {
@@ -121,78 +131,160 @@ fun SkinDetail(
                             contentDescription = "Back"
                         )
                     }
+                },
+                actions = {
+                    IconButton(onClick = {
+                        isShowSkinInfoBottomSheet = !isShowSkinInfoBottomSheet
+                    }) {
+                        // see info button
+                        Icon(
+                            imageVector = Icons.Default.Info,
+                            contentDescription = "Info"
+                        )
+                    }
+                    IconButton(onClick = {}) {
+                        // save button
+                        Icon(
+                            imageVector = Icons.Default.Save,
+                            contentDescription = "Info"
+                        )
+                    }
                 }
             )
         }
     ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding).fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            if (skinConfig != null) {
+                val motions = skinConfig.motionParams.motion
 
-        if (skinConfig != null) {
-            val motions = skinConfig.motionParams.motion
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(3),
 
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(3),
-                modifier = Modifier.padding(innerPadding),
-            ) {
-                items(motions) { motion ->
-                    val frames = motion.actions.flattenToDrawableFrames()
+                    ) {
+                    items(motions) { motion ->
+                        val frames = motion.actions.flattenToDrawableFrames()
 
-                    if (frames.isNotEmpty()) {
-                        var currentFrameIndex by remember { mutableIntStateOf(0) }
+                        if (frames.isNotEmpty()) {
+                            var currentFrameIndex by remember { mutableIntStateOf(0) }
 
-                        LaunchedEffect(motion.state) {
-                            while (true) {
-                                val duration = frames[currentFrameIndex].durationMillis.toLong()
-                                delay(duration)
-                                currentFrameIndex = (currentFrameIndex + 1) % frames.size
+                            LaunchedEffect(motion.state) {
+                                while (true) {
+                                    val duration = frames[currentFrameIndex].durationMillis.toLong()
+                                    delay(duration)
+                                    currentFrameIndex = (currentFrameIndex + 1) % frames.size
+                                }
                             }
-                        }
 
-                        Card(
-                            modifier = Modifier
-                                .padding(8.dp)
-                                .fillMaxWidth(),
-                            shape = MaterialTheme.shapes.medium,
-                            colors = CardDefaults.cardColors(
-                                containerColor = MaterialTheme.colorScheme.onPrimary,
-                                contentColor = MaterialTheme.colorScheme.onSurface,
-                            ),
-                            elevation = CardDefaults.cardElevation(
-                                defaultElevation = 4.dp,
-                                pressedElevation = 8.dp,
-                            )
-                        ) {
-                            Column(modifier = Modifier.padding(16.dp)) {
-                                Text(
-                                    text = motion.state,
-                                    style = MaterialTheme.typography.titleSmall,
-                                    modifier = Modifier
-                                        .padding(bottom = 8.dp)
-                                        .align(Alignment.CenterHorizontally)
+                            Card(
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .fillMaxWidth(),
+                                shape = MaterialTheme.shapes.medium,
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.onPrimary,
+                                    contentColor = MaterialTheme.colorScheme.onSurface,
+                                ),
+                                elevation = CardDefaults.cardElevation(
+                                    defaultElevation = 4.dp,
+                                    pressedElevation = 8.dp,
                                 )
+                            ) {
+                                Column(modifier = Modifier.padding(16.dp)) {
+                                    Text(
+                                        text = motion.state,
+                                        style = MaterialTheme.typography.titleSmall,
+                                        modifier = Modifier
+                                            .padding(bottom = 8.dp)
+                                            .align(Alignment.CenterHorizontally)
+                                    )
 
-                                DisplayLocalImage(
-                                    drawableName = frames[currentFrameIndex].drawableName,
-                                    skinDir = File(skinPath.toString()),
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .aspectRatio(1f)
-                                        .padding(bottom = 8.dp)
-                                )
+                                    DisplayLocalImage(
+                                        drawableName = frames[currentFrameIndex].drawableName,
+                                        skinDir = File(skinPath.toString()),
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .aspectRatio(1f)
+                                            .padding(bottom = 8.dp)
+                                    )
 
-                                Text(
-                                    text = "Duration: ${frames[currentFrameIndex].durationMillis} ms",
-                                    style = MaterialTheme.typography.bodyMedium,
-                                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f),
-                                    modifier = Modifier.align(Alignment.CenterHorizontally)
-                                )
+                                    Text(
+                                        text = "Duration: ${frames[currentFrameIndex].durationMillis} ms",
+                                        style = MaterialTheme.typography.bodyMedium,
+                                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f),
+                                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                                    )
+                                }
                             }
                         }
                     }
                 }
+            } else {
+                Text(
+                    text = "No skin config found",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f),
+                )
             }
         }
 
+        if (isShowSkinInfoBottomSheet) {
+            ModalBottomSheet(
+                sheetState = showSkinInfoBottomSheet,
+                onDismissRequest = { isShowSkinInfoBottomSheet = false },
+            ) {
+                Column {
+                    Text(
+                        text = "Skin Info",
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Text(
+                        text = "Skin Path: ${skinPath.toString()}",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Text(
+                        text = "Skin Name: ${skinConfig?.info?.name}",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Text(
+                        text = "Skin Author: ${skinConfig?.info?.author}",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Text(
+                        text = "Skin Description: ${skinConfig?.info?.description}",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Text(
+                        text = "Skin Id: ${skinConfig?.info?.skinId}",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Text(
+                        text = "Skin Created At: ${skinConfig?.info?.createdAt}",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Text(
+                        text = "Skin initialState: ${skinConfig?.motionParams?.initialState}",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(16.dp)
+                    )
 
+                }
+            }
+        }
     }
 }
 
